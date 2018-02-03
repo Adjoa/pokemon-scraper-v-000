215acae4c11a18a787f679863c0f0c06ca848722
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-03 21:37:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,6 +16,8 @@ class Pokemon
   def self.find(id, db)
     pk = db.execute("SELECT * FROM Pokemon WHERE id = ?", [id])
     pk_hash = {id: pk[0][0], name: pk[0][1], type:pk[0][2], db: db}
+    pk_hp =  db.execute("SELECT hp FROM Pokemon WHERE id = ?", [id])
+    binding.pry
     pk_new = Pokemon.new(pk_hash, 60)
     binding.pry
   end
