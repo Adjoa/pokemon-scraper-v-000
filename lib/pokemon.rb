072f2eb956dbdfde330794196ca5bfbeeeceaefc
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-03 21:36:57 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,8 +6,7 @@ class Pokemon
     @name = pokemon[:name]
     @type = pokemon[:type]
     @db = pokemon[:db]
-    # @hp = hp
-    #  @hp = pokemon[:hp]
+    @hp = pokemon[:hp]
   end
 
   def self.save(name, type, db)
